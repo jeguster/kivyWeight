6e67df9edbafc8ce33bf52e24e5e3cdf04846d25
jeguster <jeguster@gmail.com>
2021-05-19 13:26:06 +0300

    Created main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# WEIGHT MANAGEMENT APPLICATION MAIN
+
+# Libraries and modules
+import kivy # The Kivy framework
+kivy.require('2.0.0') # Minimum version of the framework
+from kivy.app import App # A parent class for the main window
+from kivy.uix.gridlayout import GridLayout # A parent class for the layout
+
+# Initializations and class definitions
+
+# The layout
+class Glayout(GridLayout):
+
+    # Functional parts ie methods
+    def calculate_bmi(self): # Calculates Body Mass Index (bmi)
+        pass
+    
+    def calculate_fat(self): # Calculates the fat percentage
+        pass
+
+# Create the app
+class WeightManagementApp(App):
+    def build(self):
+        return Glayout()
+    
+# Run the app
+if __name__ == "__main__":
+    # Create Application object
+    weightManagementApp = WeightManagementApp
+
+    # Run it continuously
+    weightManagementApp.run()
